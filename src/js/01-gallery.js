5fe562d8f4d863252a5dbc0c0d8a29a3ac04a0c3
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-08-12 15:50:01 +0300

    Task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    item =>
+      `<a class='gallery__item' href="${item.original}"><img class='gallery__image' src='${item.preview}' alt='${item.description}' width='408' height='272'/></a>`
+  )
+  .join('');
+
+galleryRef.innerHTML = markup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
+gallery.on('show.simplelightbox');
