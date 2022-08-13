75dd8a46ea6795412bc5ae9d4ef771e5ac727c29
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-08-13 21:31:01 +0300

    task 3 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -10,3 +11,12 @@ player.on('play', function () {
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  console.log(data.seconds);
+  localStorage.setItem('playtime', data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('playtime'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('[type="email"]');
+const messageRef = document.querySelector('[name="message"]');
+const savedInputs = localStorage.getItem('feedback-form-state');
+const formValues = {};
+
+if (savedInputs) {
+  fillInputValue();
+}
+
+formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', throttle(onFormSubmit, 500));
+
+function onFormInput(evt) {
+  formValues[evt.target.getAttribute('name')] = evt.target.value;
+
+  const formValuesJSON = JSON.stringify(formValues);
+
+  localStorage.setItem('feedback-form-state', formValuesJSON);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(`email: ${emailRef.value}, message: ${messageRef.value}`);
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function fillInputValue() {
+  if (JSON.parse(savedInputs).hasOwnProperty('email')) {
+    emailRef.value = JSON.parse(savedInputs).email;
+  }
+  if (JSON.parse(savedInputs).hasOwnProperty('message')) {
+    messageRef.value = JSON.parse(savedInputs).message;
+  }
+}
